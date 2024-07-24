7847b86a6cbfe63e0fab81a07313c19c1f7d82e9
Jesús Monda <jmonda@odilo.us>
2024-07-25 00:34:36 +0200

    fix duplicate folder

--- src/app/pages/auth/auth-login/auth-login.component.ts
+++ src/app/pages/auth/auth-login/auth-login.component.ts
@@ -1,27 +1,37 @@
 import { NgOptimizedImage, NgStyle } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { RequestService } from '../../../services/request.service';
+import { routes } from '../../../app.routes';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgOptimizedImage, RouterLink, NgStyle],
+  imports: [NgOptimizedImage, RouterLink, NgStyle, FormsModule],
   templateUrl: './auth-login.component.html',
   styleUrl: './auth-login.component.css'
 })
-export default class AuthLoginComponent implements OnInit {
-  isMobile: boolean = false;
+export default class AuthLoginComponent {
+  constructor(private router: Router, private requestService: RequestService) {}
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.checkScreenSize();
+  email:string ='';
+  password:string='';
+  status: undefined;
+
+
+  async login(){
+    try{
+      const response = await this.requestService.request('POST', `http://localhost:3000/auth/login`, {email: this.email, password:this.password}, {}, false);
+      localStorage.setItem("access_token", response.access_token);
+      this.status = undefined
+      this.router.navigate(['/home']);
+    }catch(error: any){
+      this.status = Array.isArray(error.error.message) ? error.error.message : [error.error.message];
+    }
   }
 
-  ngOnInit() {
-    this.checkScreenSize();
-  }
 
-  checkScreenSize() {
-    this.isMobile = window.innerWidth <= 767;
-  }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,48 +1,44 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private LOGIN_URL = ''; //Meter aquí el endpoint
-  private tokenKey='authToken';
 
-  constructor(private httpCLient: HttpClient, private router: Router) { }
+  constructor() { }
 
-  login(user: String, password: string){
-    return this.httpCLient.post<any>(this.LOGIN_URL, {user, password}).pipe(
-      tap(response=>{
-        if(response.token){
-          console.log(response.token);
-        }
-      })
-    )
-  }
-
-  private setToken(token:string) {
-    localStorage.setItem(this.tokenKey, token);
-  }
-
-  private getToken() {
-    return localStorage.getItem(this.tokenKey);
-  }
-
-  isAuthenticated() {
-    const token = this.getToken();
-    if(!token){
-      return false;
+  isAdmin(){
+    const access_token:string|null = localStorage.getItem('access_token')
+    if(access_token){
+      const payload:any = jwtDecode(access_token)
+      if (payload.role == "ADMIN") {
+        return true;
+      }
     }
-    //para la expiración del token
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    const exp = payload.exp * 1000;
-    return Date.now()< exp;
+    return false;
   }
 
-  logout(){
-    localStorage.removeItem(this.tokenKey);
-    this.router.navigate(['']);
+  isUser(){
+    const access_token:string|null = localStorage.getItem('access_token')
+    if(access_token){
+      const payload:any = jwtDecode(access_token)
+      if (payload.role == "USER") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isNotAuth(){
+    const access_token:string|null = localStorage.getItem('access_token')
+    if(!access_token){
+        return true;
+    }
+    return false;
   }
 }
+
