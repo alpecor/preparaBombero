d2f56ceb733a3f259f625ec3c00cec7bf55a53a7
Álvaro Pérez <alpecor@hotmail.com>
2024-08-14 13:56:00 +0200

    se soluciona el html de listado de exámenes

--- src/app/pages/admin/question-create/question-create.component.ts
+++ src/app/pages/admin/question-create/question-create.component.ts
@@ -11,8 +11,6 @@ import { FooterComponent } from '../../../components/footer/footer.component';
   styleUrl: './question-create.component.css'
 })
 export class QuestionCreateComponent {
-  @Input() isAdmin: boolean = true; //para cambiar la cabecera si es user o admin // TODO esto ya no hace falta
-
     //FUNCIONES PARA APERTURA, CIERRE y ENVIO DEL MODAL
     openModal() {
       const modalCreate = document.getElementById('createQuestion');
--- src/app/pages/examenes/examenes.component.ts
+++ src/app/pages/examenes/examenes.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ExamsInfoService } from '../../services/exams-info.service';
 
 @Component({
   selector: 'app-examenes',
   standalone: true,
   imports: [HeaderComponent,FooterComponent],
   templateUrl: './examenes.component.html',
-  styleUrl: './examenes.component.css'
+  styleUrl: './examenes.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ExamenesComponent {
 
-  @Input() isAdmin: boolean = true; //para cambiar la cabecera si es user o admin
+  data: any = {};
+
+  constructor(private examsInfoService: ExamsInfoService) {}
+
+  ngOnInit(): void {
+    this.getExamsInfo();
+  }
+
+  getExamsInfo(){
+    this.examsInfoService.getExamsInfo().subscribe( data => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
 }
